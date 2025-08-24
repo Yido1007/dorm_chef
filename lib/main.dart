5f1890b9da203595a84c62f34d236d297a1f1dc4
Yido1007 <topcuyigithan@gmail.com>
2025-08-24 21:38:24 +0300

    Firebase was integrated into the project. Called in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dorm_chef/firebase_options.dart';
 import 'package:dorm_chef/provider/grocery.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:dorm_chef/screen/home.dart';
@@ -8,6 +10,7 @@ import 'service/inventory.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PantryLocal.boot(); // Hive init + box açılışı
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
       providers: [
